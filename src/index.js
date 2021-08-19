06df04c742a8beac1f518d702359dfc837e13eed
TheDeveloperAlex <scorpion422@ukr.net>
2021-08-19 20:25:50 +0300

    Удаляет коменты

--- src/index.js
+++ src/index.js
@@ -2,14 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./Components/App";
-// import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  // (
-  // <React.StrictMode>
-  <App />,
-  document.getElementById("root")
-);
-// </React.StrictMode>,
-// document.getElementById("root")
-// );
+ReactDOM.render(<App />, document.getElementById("root"));
